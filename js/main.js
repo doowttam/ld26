245a380dce7ace88e54dc283ec40164f097a71c8
Matt Wood <doowttam@gmail.com>
2013-04-27 00:32:56 -0500

    Added gameloop boilerplate

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+OEN = (function(doc, win, $) {
+    var lastRun = Date.now();
 
+    var drawFrame = function() {
+        var now = Date.now();
+        var dt  = (now - lastRun) / 1000.0;
+
+
+        lastRun = now;
+        requestAnimationFrame(drawFrame);
+    };
+
+    return {
+        init: function() {
+            this.play();
+        },
+
+        play: function() {
+            requestAnimationFrame(drawFrame);
+        }
+    };
+}(document, window, jQuery));
+
+window.addEventListener('load', function() {
+    OEN.init();
+});
