532b388c8da2f54a7bb6f00ed10dd46405b3f0ec
Matt Wood <doowttam@gmail.com>
2013-05-07 21:16:20 -0500

    Moved some of the larger objects into their own modules

--- js/main.js
+++ js/main.js
@@ -26,123 +26,6 @@ OEN = (function(doc, win, $, map) {
     var currentWorld = 'world1';
     var tileSize     = map.tileSize;
 
-    var hero = {
-        x: 325,
-        y: 150,
-        size: 32,
-        getBounds: function(x, y) {
-            return {
-                x1: x - this.size / 2,
-                x2: x + this.size / 2,
-                y1: y - this.size / 2,
-                y2: y + this.size / 2
-            };
-        },
-        draw: function(center) {
-            var sprite = loader.asset['img/oen_sprites.png'];
-            var width = this.size;
-            var offset = 0;
-
-            if ( this.moving ) {
-                if ( Date.now() - this.lastFrame > 150 || !this.lastFrame ) {
-                    this.frame++;
-                    this.lastFrame = Date.now();
-                }
-                offset = width * (1 + this.frame % 4);
-            }
-            else {
-                this.frame = 0;
-            }
-
-            context.drawImage(sprite, offset, 0, width, width, center.x - (width/2), center.y - (width/2), width, width);
-        },
-        checkBoxCollision: function(x, y, other) {
-            var bounds = this.getBounds(x, y);
-            if (
-                bounds.x1 > other.x2    // Hero is left
-                || bounds.x2 < other.x1 // Hero is right
-                || bounds.y1 > other.y2 // Hero is below
-                || bounds.y2 < other.y1 // Hero is above
-            ) {
-                return false;
-            }
-            return true;
-        },
-        checkCollision: function(x, y) {
-            var tileCoords = coordsToTile(x, y);
-            var tiles      = getPossibleCollisionTiles(tileCoords.x, tileCoords.y);
-
-            var collides = false;
-            for (var i = 0; i < tiles.length; i++ ) {
-                var tile = tiles[i];
-                var tileCollides = this.checkBoxCollision(x, y, tile.bounds);
-                if ( tileCollides ) {
-                    collides = true;
-                }
-            }
-            return collides;
-        },
-        update: function(dt) {
-            this.moving = false;
-
-            var nextX;
-            var nextY;
-
-            if ( KEY.isDown(KEY.codes.LEFT) ) {
-                nextX = hero.x - Math.ceil(20 * dt);
-                this.moving = true;
-            }
-            if ( KEY.isDown(KEY.codes.RIGHT) ) {
-                nextX = hero.x + Math.ceil(20 * dt);
-                this.moving = true;
-            }
-            if ( KEY.isDown(KEY.codes.DOWN) ) {
-                nextY = hero.y + Math.ceil(20 * dt);
-                this.moving = true;
-            }
-            if ( KEY.isDown(KEY.codes.UP) ) {
-                nextY = hero.y - Math.ceil(20 * dt);
-                this.moving = true;
-            }
-
-            if ( typeof nextX !== 'undefined' ) {
-                if ( !this.checkCollision(nextX, hero.y) ) {
-                    hero.x = nextX;
-                }
-            }
-
-            if ( typeof nextY !== 'undefined' ) {
-                if ( !this.checkCollision(hero.x, nextY) ) {
-                    hero.y = nextY;
-                }
-            }
-        }
-    };
-
-    var loader = {
-        init: function(callback) {
-            this.finished = callback;
-
-            for ( var i = 0; i < this.images.length; i++ ) {
-                var img = new Image();
-                img.addEventListener('load', function() {
-                    loader.onLoad();
-                });
-                img.src = this.images[i];
-                this.asset[this.images[i]] = img;
-            }
-        },
-        loaded: 0,
-        images: [ 'img/oen_sprites.png' ],
-        asset: {},
-        onLoad: function() {
-            this.loaded++;
-            if ( this.loaded >= this.images.length ) {
-                this.finished();
-            }
-        }
-    };
-
     var hud = {
         size: 50,
         init: function() {
@@ -164,72 +47,13 @@ OEN = (function(doc, win, $, map) {
         var dt  = (now - lastRun) / 1000.0;
         canvas.width = canvas.width;
 
-        hero.update(dt);
+        OEN.hero.update(dt);
         drawMap();
 
         lastRun = now;
         requestAnimationFrame(drawFrame);
     };
 
-    var coordsToTile = function(x, y) {
-        var xOffset = x % tileSize;
-        var xTile = (x - xOffset) / tileSize;
-
-        var yOffset = y % tileSize;
-        var yTile = (y - yOffset) / tileSize;
-
-        return {
-            x: xTile,
-            y: yTile
-        };
-    };
-
-    var getPossibleCollisionTiles = function(x, y) {
-        var world = map[currentWorld];
-
-        var tiles = [];
-        for (var iy = y - 1; iy <= y + 1; iy++) {
-            // Check if row exists
-            var row  = world[iy];
-            if ( typeof row === 'undefined' ) {
-                continue;
-            }
-
-            for (var ix = x - 1; ix <= x + 1; ix++) {
-                if ( ix == x && iy == y ) {
-                    continue;
-                }
-
-                var tile = row[ix];
-                if ( typeof tile === 'undefined' ) {
-                    continue;
-                }
-
-                var props = map.tiles[tile];
-
-                if ( props.collide ) {
-                    var bounds = map.getBounds(ix, iy);
-                    tiles.push({x: ix, y: iy, props: props, bounds: bounds});
-                }
-            }
-        }
-
-        return tiles;
-    };
-
-    var getCoords = function(coord, center) {
-        var mapTileOffset  = coord % tileSize;
-        var viewportOffset = center % tileSize;
-
-        var offsetTiles = Math.ceil(center / tileSize);
-        var mapTile     = (coord - mapTileOffset) / tileSize;
-
-        var tileCoord  = -1 * (viewportOffset + mapTileOffset);
-        return {
-            firstTileCoord: tileCoord,             // The coord where we start drawing tiles
-            firstTile:      mapTile - offsetTiles, // The first tile of the map to draw
-        };
-    };
 
     var drawMap = function() {
         var center = {
@@ -237,8 +61,8 @@ OEN = (function(doc, win, $, map) {
             y: canvas.height / 2
         };
 
-        var coordsX = getCoords(hero.x, center.x);
-        var coordsY = getCoords(hero.y, center.y);
+        var coordsX = OEN.getCoords(OEN.hero.x, center.x);
+        var coordsY = OEN.getCoords(OEN.hero.y, center.y);
 
         var tileY = coordsY.firstTile;
         for (var y = coordsY.firstTileCoord; y < canvas.height; y += tileSize) {
@@ -252,14 +76,14 @@ OEN = (function(doc, win, $, map) {
             tileY += 1;
         }
 
-        hero.draw(center);
+        OEN.hero.draw(center);
     };
 
     var drawBGTile = function(vpX, vpY) {
         // Draw the middle tree texture, it'll serve as a background
         var props   = map.tiles[2];
         var texture = props.texture;
-        var sprite  = loader.asset['img/oen_sprites.png'];
+        var sprite  = OEN.loader.asset['img/oen_sprites.png'];
         context.drawImage(sprite, texture.offsetX, texture.offsetY, tileSize, tileSize, vpX, vpY, tileSize, tileSize);
     }
 
@@ -282,7 +106,7 @@ OEN = (function(doc, win, $, map) {
 
             if ( props.texture ) {
                 var texture = props.texture;
-                var sprite  = loader.asset['img/oen_sprites.png'];
+                var sprite  = OEN.loader.asset['img/oen_sprites.png'];
                 context.drawImage(sprite, texture.offsetX, texture.offsetY, tileSize, tileSize, vpX, vpY, tileSize, tileSize);
             }
             else {
@@ -317,6 +141,8 @@ OEN = (function(doc, win, $, map) {
             canvas  = doc.getElementById('game-canvas');
             context = canvas.getContext('2d');
 
+            OEN.hero.init(canvas, context);
+
             win.addEventListener('keyup', function(e) {
                 KEY.onKeyUp(e);
             });
@@ -325,16 +151,201 @@ OEN = (function(doc, win, $, map) {
                 KEY.onKeyDown(e);
             });
 
-            loader.init(function() {
+            OEN.loader.init(function() {
                 OEN.play();
             })
 
             hud.init();
         },
 
+        coordsToTile:  function(x, y) {
+            var xOffset = x % tileSize;
+            var xTile = (x - xOffset) / tileSize;
+
+            var yOffset = y % tileSize;
+            var yTile = (y - yOffset) / tileSize;
+
+            return {
+                x: xTile,
+                y: yTile
+            };
+        },
+
+        getPossibleCollisionTiles: function(x, y) {
+            var world = map[currentWorld];
+
+            var tiles = [];
+            for (var iy = y - 1; iy <= y + 1; iy++) {
+                // Check if row exists
+                var row  = world[iy];
+                if ( typeof row === 'undefined' ) {
+                    continue;
+                }
+
+                for (var ix = x - 1; ix <= x + 1; ix++) {
+                    if ( ix == x && iy == y ) {
+                        continue;
+                    }
+
+                    var tile = row[ix];
+                    if ( typeof tile === 'undefined' ) {
+                        continue;
+                    }
+
+                    var props = map.tiles[tile];
+
+                    if ( props.collide ) {
+                        var bounds = map.getBounds(ix, iy);
+                        tiles.push({x: ix, y: iy, props: props, bounds: bounds});
+                    }
+                }
+            }
+
+            return tiles;
+        },
+
+        getCoords: function(coord, center) {
+            var mapTileOffset  = coord % tileSize;
+            var viewportOffset = center % tileSize;
+
+            var offsetTiles = Math.ceil(center / tileSize);
+            var mapTile     = (coord - mapTileOffset) / tileSize;
+
+            var tileCoord  = -1 * (viewportOffset + mapTileOffset);
+            return {
+                firstTileCoord: tileCoord,             // The coord where we start drawing tiles
+                firstTile:      mapTile - offsetTiles, // The first tile of the map to draw
+            };
+        },
+
         play: function() {
             requestAnimationFrame(drawFrame);
             hud.draw();
+        }
+    };
+}(document, window, jQuery, MAP));
+
+OEN.loader = (function(doc, win, $, map) {
+    return {
+        init: function(callback) {
+            this.finished = callback;
+
+            for ( var i = 0; i < this.images.length; i++ ) {
+                var img = new Image();
+                img.addEventListener('load', function() {
+                    OEN.loader.onLoad();
+                });
+                img.src = this.images[i];
+                this.asset[this.images[i]] = img;
+            }
+        },
+        loaded: 0,
+        images: [ 'img/oen_sprites.png' ],
+        asset: {},
+        onLoad: function() {
+            this.loaded++;
+            if ( this.loaded >= this.images.length ) {
+                this.finished();
+            }
+        }
+    };
+}(document, window, jQuery, MAP));
+
+OEN.hero = (function(doc, win, $, map) {
+    return {
+        x: 325,
+        y: 150,
+        size: 32,
+        init: function(canvas, context) {
+            this.canvas  = canvas;
+            this.context = context;
+        },
+        getBounds: function(x, y) {
+            return {
+                x1: x - this.size / 2,
+                x2: x + this.size / 2,
+                y1: y - this.size / 2,
+                y2: y + this.size / 2
+            };
+        },
+        draw: function(center) {
+            var sprite = OEN.loader.asset['img/oen_sprites.png'];
+            var width = this.size;
+            var offset = 0;
+
+            if ( this.moving ) {
+                if ( Date.now() - this.lastFrame > 150 || !this.lastFrame ) {
+                    this.frame++;
+                    this.lastFrame = Date.now();
+                }
+                offset = width * (1 + this.frame % 4);
+            }
+            else {
+                this.frame = 0;
+            }
+
+            this.context.drawImage(sprite, offset, 0, width, width, center.x - (width/2), center.y - (width/2), width, width);
+        },
+        checkBoxCollision: function(x, y, other) {
+            var bounds = this.getBounds(x, y);
+            if (
+                bounds.x1 > other.x2    // Hero is left
+                || bounds.x2 < other.x1 // Hero is right
+                || bounds.y1 > other.y2 // Hero is below
+                || bounds.y2 < other.y1 // Hero is above
+            ) {
+                return false;
+            }
+            return true;
+        },
+        checkCollision: function(x, y) {
+            var tileCoords = OEN.coordsToTile(x, y);
+            var tiles      = OEN.getPossibleCollisionTiles(tileCoords.x, tileCoords.y);
+
+            var collides = false;
+            for (var i = 0; i < tiles.length; i++ ) {
+                var tile = tiles[i];
+                var tileCollides = this.checkBoxCollision(x, y, tile.bounds);
+                if ( tileCollides ) {
+                    collides = true;
+                }
+            }
+            return collides;
+        },
+        update: function(dt) {
+            this.moving = false;
+
+            var nextX;
+            var nextY;
+
+            if ( KEY.isDown(KEY.codes.LEFT) ) {
+                nextX = this.x - Math.ceil(20 * dt);
+                this.moving = true;
+            }
+            if ( KEY.isDown(KEY.codes.RIGHT) ) {
+                nextX = this.x + Math.ceil(20 * dt);
+                this.moving = true;
+            }
+            if ( KEY.isDown(KEY.codes.DOWN) ) {
+                nextY = this.y + Math.ceil(20 * dt);
+                this.moving = true;
+            }
+            if ( KEY.isDown(KEY.codes.UP) ) {
+                nextY = this.y - Math.ceil(20 * dt);
+                this.moving = true;
+            }
+
+            if ( typeof nextX !== 'undefined' ) {
+                if ( !this.checkCollision(nextX, this.y) ) {
+                    this.x = nextX;
+                }
+            }
+
+            if ( typeof nextY !== 'undefined' ) {
+                if ( !this.checkCollision(this.x, nextY) ) {
+                    this.y = nextY;
+                }
+            }
         }
     };
 }(document, window, jQuery, MAP));
